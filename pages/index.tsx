4a7d594e8b4d48386684ec57a18f6e6a24678c07
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-11-08 00:37:59 +0600

    creating PostCaard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12"></div>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1 bg-yellow-700">
+          {posts.map((post, i) => (
+            <>
+              <div className="" key={i}>
+                {post.title}
+              </div>
+            </>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8 bg-amber-400 h-20"></div>
+        </div>
+      </div>
     </div>
   );
 };
