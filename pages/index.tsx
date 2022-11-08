772d51c95b80d7fffaad6c4393c0ce8dc5818c7b
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-11-08 23:40:41 +0600

    creating header component & add css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Categories, PostCard, PostWidget } from "../components";
 
 const posts = [
   {
@@ -17,17 +18,16 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1 bg-yellow-700">
-          {posts.map((post, i) => (
-            <>
-              <div className="" key={i}>
-                {post.title}
-              </div>
-            </>
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post: any) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8 bg-amber-400 h-20"></div>
+          <div className="lg:sticky relative top-8 bg-amber-400 h-20">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
